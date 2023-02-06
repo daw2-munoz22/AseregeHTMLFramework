71d7f73a4eee790f7324b03692f1f9f6bcf329d2
daw2-munoz22 <91275885+daw2-munoz22@users.noreply.github.com>
2023-02-06 20:38:57 +0100

    inicial

--- src/javaapplication1/HtmlBuilder.java
+++ src/javaapplication1/HtmlBuilder.java
@@ -14,16 +14,30 @@ import java.io.OutputStream;
  * @author Edgar Muñoz
  */
 public class HtmlBuilder implements HttpHandler {    
+   
+    private String pageName;
+    
+    private DOMHtml header;
     private DOMHtml css;
     private DOMHtml html;
+    private DOMHtml footer;
     
-    public HtmlBuilder (DOMHtml css, DOMHtml html){
-        this.css = css;
+    
+    public HtmlBuilder (DOMHtml header, DOMHtml css, DOMHtml html, DOMHtml footer){
+        this.header = header;        
+        this.css = css;        
         this.html = html;
+        this.footer = footer;
     }
-    
-    public void handle(HttpExchange t) throws IOException {            
-        String response = "<html><head><style>" + css.GetDom() + "</style></head><body>" + html.GetDom() + "</body></html>";        
+    public void SetPageName(String pageName) {
+        this.pageName = pageName;        
+    }
+    public void handle(HttpExchange t) throws IOException {   
+        
+        if (pageName.equals("update")){
+            JavaApplication1.pages.UpdatePages();                
+        }
+        String response = "<html><head>" + header.GetDom() + "<style>" + css.GetDom() + "</style></head><body>" + html.GetDom() + "<footer>" + footer.GetDom() + "</footer></body></html>";        
         t.sendResponseHeaders(200, response.length());        
         OutputStream os = t.getResponseBody();        
         os.write(response.getBytes());        
