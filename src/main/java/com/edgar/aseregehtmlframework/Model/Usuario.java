86f922b8807cca43d585433c45dda078c0df8dad
daw2-munoz22 <91275885+daw2-munoz22@users.noreply.github.com>
2023-02-18 23:26:19 +0100

    Progreso_Dia_18/02/2023_AES, Encriptar password Base de Datos, Diagrama de clases y su distribución

---
+++ src/main/java/com/edgar/aseregehtmlframework/Model/Usuario.java
@@ -0,0 +1,61 @@
+package com.edgar.aseregehtmlframework.Model;
+
+/**
+ *
+ * @author Edgar Muñoz
+ */
+public class Usuario {
+    private int idUsuario;
+    private String nombre;
+    private String apellido;
+    private int edad;
+    private String sexo;
+    private String password;
+    
+    public Usuario(int idUsuario, String nombre, String apellido, int edad, String sexo, String password){
+        this.idUsuario = idUsuario;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.edad = edad;
+        this.sexo = sexo;
+        this.password = password;
+    }
+    public int getIdUsuario(){
+        return idUsuario;
+    }
+    public String getNombre() {
+        return nombre;
+    }
+    public String getApellido() {
+        return apellido;
+    } 
+    public int getEdad(){
+        return edad;
+    }
+    public String getSexo() {
+        return sexo;
+    } 
+    public String getPassword() {
+        return password;
+    }
+    
+    public void SetIdUsuario(int idUsuario){
+        this.idUsuario = idUsuario;        
+    }
+    public void SetNombre(String nombre){
+        this.nombre = nombre;        
+    }
+    
+    public void SetApellido(String apellido){
+        this.apellido = apellido;        
+    }
+    public void SetEdad(int edad){
+        this.edad = edad;
+    }
+    public void SetSexo(String sexo){
+        this.sexo = sexo;        
+    }
+    public void SetPassword(String password){
+        this.password = password;        
+    }
+}
