620fb5902210581c15910e8973dd7587618c097b
daw2-munoz22 <91275885+daw2-munoz22@users.noreply.github.com>
2023-05-20 13:12:35 +0200

    Mejorar Vista Quienes Somos, COMPLETADO

---
+++ AseregeBarcelonaWeb.Model/Data/Paragraph.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace AseregeBarcelonaWeb.Model.Data
+{
+	public class Paragraph
+	{
+		private string title;			
+		private List<string> paragraph;
+
+		public Paragraph(string title, List<string> paragraph)
+		{
+			this.title = $"<h2>{System.Net.WebUtility.HtmlEncode(title)}</h2>";
+			this.paragraph = paragraph;
+								
+		}
+		public string GetParagraph() 
+		{
+			string text = title;
+			foreach (string line in paragraph)
+			{
+				text = string.Concat(text, $"<p>{System.Net.WebUtility.HtmlEncode(line)}</p>");
+			}
+			return text;
+		}
+	}
+}
